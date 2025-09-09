9498b42cc9f88d1e307477f576b5046522358d5a
Dominique <disland14@gmail.com>
2025-09-09 12:21:59 -0500

    ticket numbet

---
+++ terraform-revision/main.tf
@@ -0,0 +1,13 @@
+resource "aws_iam_user" "name" {
+  name = "dianna"
+}
+resource "aws_iam_group" "group" {
+  name = "sre"
+}
+
+resource "aws_iam_group_membership" "name" {
+    name = "code"
+  group = aws_iam_group.group.name 
+  users = [aws_iam_user.name.name ]
+}
+
